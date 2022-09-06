10a0d9d8dc0d35e2b23652b265ad275280bf1b53
AdvayCoder <palnitkaradvay@gmail.com>
2022-09-06 22:07:23 +0530

    Created basic view and layout of the app.

---
+++ src/main/java/com/asavahtech/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.asavahtech.calculator;
+import com.asavahtech.calculator.view.MainFrame;
+/**
+ *
+ * @author advay
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+    }
+}
